0dc46c6978c1933d9f09cc62e4d14cbe082890fa
Mezea11 <christiancastellanosmeza@gmail.com>
2023-10-16 20:18:08 +0200

    Fourth commit, more exercises

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 //Användare får knappa in något i pop-up ruta
 let userInput = prompt("Skriv in något");
 
@@ -5,7 +6,7 @@ let userInput = prompt("Skriv in något");
 console.log("Lite kod");
 //Allting efter två slashes blir kommentar
 
-/* 
+ 
 Detta
 gör
 att
@@ -13,7 +14,7 @@ flera
 rader
 blir
 kommentar
-*/
+
 
 // Textsträng
 String = "Hello world";
@@ -122,3 +123,56 @@ console.log(name);
 for (let i = 1; i <= 100; i++) {
     console.log(i);
 }
+*/
+
+let myName = "Christian";
+console.log(myName);
+
+let myAge = "Ironman";
+console.log(myAge);
+
+let numberArray = [1, 2, 3, 4, 5];
+console.log(numberArray)
+
+let number1 = (1);
+let number2 = (2);
+
+console.log(number1, number2);
+
+
+let opposite = Boolean = false;
+console.log(!opposite);
+
+let a = "Hej!";
+let b = 1;
+let c = true;
+let d = false;
+let e = {};
+let f = [];
+let g = [1, "Hej"];
+let h = "7";
+let i = "true";
+let j = true && true;
+let k = 1 + 0.3;
+let l = "1" + 2;
+let m = { a: 1 };
+let n = [{ a: 1 }];
+let o = { a: [1] };
+let p = { a: true || 1 < 3 };
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);
+console.log(typeof h);
+console.log(typeof i);
+console.log(typeof j);
+console.log(typeof k);
+console.log(typeof l);
+console.log(typeof m);
+console.log(typeof n);
+console.log(typeof o);
+console.log(typeof p);
