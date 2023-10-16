2790d7b2020f945525b48a7d958d9793345f0e9f
Mezea11 <christiancastellanosmeza@gmail.com>
2023-10-16 18:07:18 +0200

    Second commit, learning syntax

---
+++ main.js
@@ -0,0 +1,47 @@
+//Användare får knappa in något i pop-up ruta
+let userInput = prompt("Skriv in något");
+
+//Kommentar
+console.log("Lite kod");
+//Allting efter två slashes blir kommentar
+
+/* 
+Detta
+gör
+att
+flera
+rader
+blir
+kommentar
+*/
+
+// Textsträng
+String = "Hello world";
+
+// Nummer 
+
+Number = 1;
+
+// Boolean - Innehåller true/false värden
+Boolean = true;
+Boolean = false;
+
+// Array - lagrar saker som siffror/text
+Array [1, 2 ,3];
+
+//Object - datatyp
+
+
+//Undefined och null - signalerar att ett värde inte kan hittas/finns
+
+let myVariable; //Denna får undefined eftersom ett värde inte finns
+
+
+
+//ARRAY FORTS
+
+let emptyArray[]; //Tom array
+let arrayOfNumbers = [1, 2, 3, 4]; //Nummer array
+let arrayOfStrings = ["Ett", "Två", "Tre", "Fyra"]; //Sträng array
+let arrayOfMixedTypes = [1, "Ett", 2, "Två", 3, "Tre", 4, "Fyra"]; // Nummer och sträng array
+let arrayOfArrays = [[true, 1, "Ett"], [1, 2, 3] ["Ett", "Två", "Tre"]] // Nummer, sträng och boolean array
